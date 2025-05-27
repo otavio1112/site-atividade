a141771efe131d7c818027d591d0437a5b740bfd
otavio1112 <otaviogomesbvp@gmail.com>
2025-05-27 15:34:40 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,35 @@
 criaCartao(
+    'Pergunta 1',
     'QUANDO O VALORANT FOI LANÇADO?',
     '2020.'
 )
 
 criaCartao(
+    'Pergunta 2',
     'Em quais classes são divididos os agentes?',
     'São divididos em: Duelistas, controladores, inicializadores e sentinelas'
 )
 
 criaCartao(
+    'Pergunta 3',
     'Quantos mapas tem?',
     'Possue 9 mapas sendo, Ascente, Pearl, Haven, Fracture, Abyss, Bind, Breeze, Lotus e a Icebox'
 )
 
 criaCartao(
+    'Pergunta 4',
     'Qual foi o primeiro pacote de skin com animação?',
     'Foi o pacote elderflame'
 )
 
 criaCartao(
+    'Pergunta 5',
     'Quem foi o MVP do champions 2022?',
     'O MPV de 2022 foi o PANCADA que venceu o champions com a LOUD'
 )
 
 criaCartao(
+    'Pergunta 6',
     'Quem é reconhecido como o melhor jogador de valorant profissional?',
-    'Para encontrar um dos lados de um triângulo retângulo, sendo dado os outros dois lados. h² = c² + c².'
+    'Erick Santos, mais conhecido como "ASPAS".'
 )
